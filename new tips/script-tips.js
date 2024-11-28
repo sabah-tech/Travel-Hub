a39ab9d18c6c607b5239165b0bfb2b6ea74135d1
amjady7 <alyousif.amjad@gmail.com>
2024-11-28 21:07:46 +0200

    logo insertion

--- new tips/script-tips.js
+++ new tips/script-tips.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const navbar = document.querySelector(".navbar");
-    const hamburger = document.querySelector(".hamburger");
-    navbar.classList.toggle("open");
-    hamburger.classList.toggle("active");
-  }
+  let navbar = document.querySelector(".navbar");
+  let hamburger = document.querySelector(".hamburger");
+  navbar.classList.toggle("open");
+  hamburger.classList.toggle("active");
+}
