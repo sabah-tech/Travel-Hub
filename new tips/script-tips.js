5609118f4afbaaa5628e072d8b416aec78ba62c7
amjady7 <alyousif.amjad@gmail.com>
2024-11-28 16:19:51 +0200

    adding tips page

---
+++ new tips/script-tips.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const navbar = document.querySelector(".navbar");
+    const hamburger = document.querySelector(".hamburger");
+    navbar.classList.toggle("open");
+    hamburger.classList.toggle("active");
+  }
